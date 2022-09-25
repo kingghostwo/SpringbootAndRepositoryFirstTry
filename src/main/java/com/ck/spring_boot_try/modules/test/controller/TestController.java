9bdbee38eb605d7512f8715fffb17bb3032173ad
kingghostwo <914262938@qq.com>
2022-09-25 13:39:40 +0800

    endpoint

--- src/main/java/com/ck/spring_boot_try/modules/test/controller/TestController.java
+++ src/main/java/com/ck/spring_boot_try/modules/test/controller/TestController.java
@@ -10,12 +10,21 @@ import com.ck.spring_boot_try.modules.test.entity.ApplicationTest;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -86,6 +95,45 @@ public class TestController {
     public String testDesc(HttpServletRequest request, @RequestParam String key){
         String key2 = request.getParameter("key");
         return "this is test desc." + key +"---" + key2;
+    }
 
+    @PostMapping(value = "/file", consumes = "multipart/form-data")
+    public String uploadFile(@RequestParam MultipartFile file, RedirectAttributes redirectAttributes){
+        if (file.isEmpty()){
+//            addAttribute是明文
+            redirectAttributes.addFlashAttribute("message","Please select file.");
+//            跳转到另一个控制器，不是页面
+            return "redirect:/test/index";
+        }
+        try {
+            String fileName = file.getOriginalFilename();
+            File destFile = new File(
+                    "D:\\project\\Java\\CKHOMEWORK\\SpringbootAndRepositoryFirstTry\\target\\classes\\static\\upload/"
+                            +fileName);
+            file.transferTo(destFile);
+
+
+            redirectAttributes.addFlashAttribute("message","Upload file success");
+        } catch (IOException e) {
+            redirectAttributes.addFlashAttribute("message","Upload file failed");
+            e.printStackTrace();
+        }
+        return "redirect:/test/index";
+    }
+
+    @RequestMapping("/download")
+    public ResponseEntity<Resource> downLoadFile(@RequestParam String fileName) {
+        try {
+            Resource resource = new UrlResource(Paths.get("D:\\project\\Java\\CKHOMEWORK\\SpringbootAndRepositoryFirstTry\\target\\classes\\static\\upload/"
+                    + fileName).toUri());
+            return ResponseEntity.ok()
+                    .header(HttpHeaders.CONTENT_TYPE, "application/octet-stream")
+                    .header(HttpHeaders.CONTENT_DISPOSITION,
+                            String.format("attachment; filename=\"%s\"", resource.getFilename()))
+                    .body(resource);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
